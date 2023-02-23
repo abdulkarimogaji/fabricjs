86e14e19a93db25cc1c47986329d6ea973b9a258
Abdulkarim Ogaji <abdulkarimogaji001@gmail.com>
2023-02-23 16:34:57 +0100

    add effects slider, wip simple photo

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const COUNTDOWN = document.getElementById("countdown");
 const COUNTDOWN_TEXT = document.getElementById("countdown-text");
 const CANVAS_LABEL = document.getElementById("canvas-label");
 const CANVAS_CONTAINER = document.getElementById("canvas-container");
+const VIDEO = document.getElementById("video");
 const captureBtn = document.getElementById("capture-btn");
 
-const testButton = document.getElementById("test-button");
+const VENDOR_URL = window.URL || window.webkitURL;
+const shutterSound = new Audio();
+shutterSound.autoplay = true;
+let FLIP_MODE = true;
 
 // constants
 const MODES = {
@@ -27,18 +31,39 @@ const SCREENS = {
   SELECT_EMAIL_OR_PHONE: "SELECT_EMAIL_OR_PHONE",
   EMAIL_FORM: "EMAIL_FORM",
   PHONE_FORM: "PHONE_FORM",
+  SOCIAL_SHARE: "SOCIAL_SHARE",
 };
-
+let WEBCAM_ON = false;
 let IN_COUNTDOWN = false;
 let IN_SCREEN_CHANGE = false;
 let CURRENT_MODE = MODES.NONE;
 let CURRENT_SCREEN = SCREENS.SELECT_MODE;
+
+function mobileAndTabletCheck() {
+  let check = false;
+  (function (a) {
+    if (
+      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
+        a
+      ) ||
+      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+        a.substr(0, 4)
+      )
+    )
+      check = true;
+  })(navigator.userAgent || navigator.vendor || window.opera);
+  return check;
+}
 
 function startLoader() {
   LOADER.classList.remove("hidden");
 }
 function stopLoader() {
   LOADER.classList.add("hidden");
+}
+
+function playShutterSound() {
+  shutterSound.src = "../assets/shutter_sound.mp3";
 }
 
 function sleep(time) {
@@ -72,6 +97,13 @@ function startCountdown(sec) {
 
 async function changeScreen(screenType, direction) {
   if (CURRENT_SCREEN == screenType || IN_SCREEN_CHANGE) return;
+  // stop webcam on other screens
+  if (
+    ![SCREENS.SELECT_MODE, SCREENS.CAPTURE_SCREEN].includes(screenType) &&
+    WEBCAM_ON
+  ) {
+    stopWebcam();
+  }
   startLoader();
   IN_SCREEN_CHANGE = true;
   console.log("SCREEN_CHANGE: ", screenType);
@@ -88,8 +120,13 @@ async function changeScreen(screenType, direction) {
   });
 
   switch (screenType) {
+    case SCREENS.SELECT_MODE:
+      CANVAS_LABEL.innerText = "Choose your favorite photo experience";
+      await startWebcam();
+      break;
     case SCREENS.SELECT_BACKGROUND:
       CANVAS_LABEL.innerText = "Select a Background";
+      await fetchOverlays();
       break;
     case SCREENS.CONFIRM_CAPTURE:
       CANVAS_LABEL.innerText = "Do you like it?";
@@ -108,6 +145,10 @@ async function changeScreen(screenType, direction) {
       CANVAS_LABEL.innerText =
         "Would you like to receive it by email or texted to your phone?";
       break;
+    case SCREENS.SOCIAL_SHARE:
+      CANVAS_CONTAINER.classList.add("hidden");
+      CANVAS_LABEL.innerText = "";
+      break;
     default:
       CANVAS_LABEL.innerText = "";
   }
@@ -117,7 +158,31 @@ async function changeScreen(screenType, direction) {
   stopLoader();
 }
 
+async function takePicture() {
+  return new Promise((resolve, reject) => {
+    const camCanvas = document.createElement("canvas");
+    const context = camCanvas.getContext("2d", { willReadFrequently: true });
+    camCanvas.width = 1080;
+    camCanvas.height = 1080;
+    if (mobileAndTabletCheck() && FLIP_MODE) {
+      context.translate(1080, 0);
+      context.scale(-1, 1);
+    } else if (FLIP_MODE) {
+      context.translate(1080, 0);
+      context.scale(-1, 1);
+    }
+    context.drawImage(VIDEO, 0, 0, 1080, 1080);
+    const data = camCanvas.toDataURL("image/png", 0.4);
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, 1080, 1080);
+    resolve(data);
+  });
+}
+
 async function photoCapture() {
+  await startCountdown(3);
+  playShutterSound();
+  await takePicture();
   changeScreen(SCREENS.CONFIRM_CAPTURE);
 }
 
@@ -129,9 +194,39 @@ async function threeShotGifCapture() {
   changeScreen(SCREENS.CONFIRM_CAPTURE);
 }
 
-START_BUTTON.addEventListener("click", () => {
+async function startWebcam() {
+  const facingMode = FLIP_MODE ? "user" : "environment";
+  if (!navigator.mediaDevices.getUserMedia) return;
+  try {
+    VIDEO.srcObject = await navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode,
+        aspectRatio: 1,
+      },
+    });
+  } catch (err) {
+    console.log("ERROR STARTING WEBCAM", err.message);
+  }
+  WEBCAM_ON = true;
+}
+
+function stopWebcam() {
+  var stream = VIDEO.srcObject;
+  if (stream) {
+    var tracks = stream.getTracks();
+    for (var i = 0; i < tracks.length; i++) {
+      var track = tracks[i];
+      track.stop();
+    }
+    VIDEO.srcObject = null;
+  }
+  WEBCAM_ON = false;
+}
+
+START_BUTTON.addEventListener("click", async () => {
   APP_SCREEN.classList.remove("hidden");
   START_BUTTON.classList.add("hidden");
+  await startWebcam();
 });
 
 // event listeners for selecting mode
